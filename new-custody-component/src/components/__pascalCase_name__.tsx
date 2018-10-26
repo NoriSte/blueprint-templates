714d5296975e4f82fd10c98a59d40b780c96912a
Stefano Magni <nori.ste.magni@gmail.com>
2018-10-26 11:10:32 +0200

    react and typescript template for a project of Conio

---
+++ new-custody-component/src/components/__pascalCase_name__.tsx
@@ -0,0 +1,44 @@
+// eslint-disable
+
+import { AppState } from '@/data';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
+
+
+interface Props {
+  aSimpleProp?: string,
+  aCallbackProp?: (data:{}) => void
+};
+
+function mapStateToProps(state: AppState) {
+  return {
+    // token: state.auth.token,
+  };
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  return {
+    // both for sync or async action creators
+    // loadList: (params?:{}) => dispatch(actionName(params)),
+  };
+};
+
+interface State = {
+
+}
+
+class {{pascalCase name}} extends React.PureComponent<Props, State> {
+  /*
+  public  static getDerivedStateFromProps(nextProps: Props, prevState: State) {
+
+  }
+  */
+
+  public render() {
+    return <div>{JSON.stringify(this.props, null, 2)}</div>;
+  }
+}
+
+// export default {{pascalCase name}};
+export default connect(mapStateToProps, mapDispatchToProps)({{pascalCase name}});
